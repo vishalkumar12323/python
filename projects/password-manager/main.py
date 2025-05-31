ac1ea054e78ed05a4954a864ad2497bc58ba089d
Vishal Kumar <vishalninaniya8949info@gmail.com>
2025-05-31 22:23:32 +0530

    completed password-manager, added flash-card-project

--- projects/password-manager/main.py
+++ projects/password-manager/main.py
@@ -22,18 +22,21 @@ def generate_password():
     pyperclip.copy(password)
 
 
-def search_password():
+def find_password():
     website_name = website_entry.get()
     if len(website_name) != 0:
-        with open("data.json", 'r') as data_file:
-            data = json.load(data_file)
-            try:
-                result = data[website_name]
-            except KeyError:
-                messagebox.showerror(title="Not found error ", message=f'email with password does not found for the website {website_name}.')
-                return
-            else:
-                messagebox.showinfo(title='password fetched', message=f"{website_name} website email or password.\n Email: {result['email']}\n, Password: {result['password']}.")
+        try:
+            with open("data.json", 'r') as data_file:
+                data = json.load(data_file)
+                try:
+                    result = data[website_name]
+                except KeyError:
+                    messagebox.showerror(title="Not found", message=f'email with password does not found for the website {website_name}.')
+                    return
+                else:
+                    messagebox.showinfo(title=f'{website_name}', message=f"Email: {result['email']}\n Password: {result['password']}")
+        except FileNotFoundError:
+            messagebox.showerror(title="Error", message="No Data File Found")
     else:
         messagebox.showerror(title="Error ", message="Please enter the website name that you want to search email or password.")
         return
@@ -56,7 +59,7 @@ def save_credenial():
             with open("./data.json", "r") as read_file:
                 data = json.load(read_file)
 
-        except FileNotFoundError as err:
+        except FileNotFoundError:
             with open("./data.json", 'w') as write_file:
                 json.dump(new_data, write_file, indent=4)
         else:
@@ -77,29 +80,30 @@ logo = PhotoImage(file="./logo.png")
 canvas.create_image(100, 100, image=logo)
 canvas.grid(row=0, column=1)
 
-# setup labels
-website_label = Label(text="Website", pady=5)
+#Labels
+website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username")
+email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-password_label = Label(text="Password")
+password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-# entries
-website_entry = Entry(width=35)
+#Entries
+website_entry = Entry(width=21)
 website_entry.grid(row=1, column=1)
 website_entry.focus()
-
-search_button = Button(text="Search Password", command=search_password)
-search_button.grid(row=1, column=2)
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "email@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
-# buttons
-generate_pass_button = Button(text="Generate Password", command=generate_password)
-generate_pass_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, pady=5, command=save_credenial)
+# Buttons
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
+generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=save_credenial)
 add_button.grid(row=4, column=1, columnspan=2)
+
 window.mainloop()
